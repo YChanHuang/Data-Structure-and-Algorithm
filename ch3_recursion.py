32a80ab51e94e7c3713813902b2cb938e2a4b380
James Huang <41849932+YChanHuang@users.noreply.github.com>
2024-12-29 21:39:46 +0000

    Adding Ch3 coding examples

---
+++ ch3_recursion.py
@@ -0,0 +1,12 @@
+def fact(x):
+    if x == 1:
+        return 1 
+    else:
+        return x * fact(x - 1)
+
+
+
+fact(4)
+
+# I've used this website to help me undedstand stack visually 
+# https://is.gd/WE2FE9``
