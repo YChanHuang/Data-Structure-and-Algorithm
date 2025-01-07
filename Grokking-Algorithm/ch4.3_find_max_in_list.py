18aa73843202f1ad2e1b1c7bfcdb66204c5cc454
James Huang <41849932+YChanHuang@users.noreply.github.com>
2025-01-07 22:14:55 +0000

    I had another attempt to write the recursive max on my own

--- Grokking-Algorithm/ch4.3_find_max_in_list.py
+++ Grokking-Algorithm/ch4.3_find_max_in_list.py
@@ -1,9 +1,10 @@
-# Write a recursive function to find the maximum number in a list.
+def my_max(lst):
+    if len(lst) == 1:
+        return lst[0]  # Base case: If there's only one element, return it
+    else:
+        max_of_rest = my_max(lst[1:])
+        return lst[0] if lst[0] > max_of_rest else max_of_rest
 
-def my_max(list):
-  if len(list) == 2:
-    return list[0] if list[0] > list[1] else list[1]
-  sub_max = my_max(list[1:])
-  return list[0] if list[0] > sub_max else sub_max
 
+print(my_max([1, 6, 99, 10, 333, 666, 7777, 2, 4, 6]))])
 
